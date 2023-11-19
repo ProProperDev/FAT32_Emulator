fcb9596bc832657c1525aa833824efffe2dbfc27
ProProperDev <137059053+ProProperDev@users.noreply.github.com>
2023-11-19 22:35:10 +0400

    Add files via upload

--- print_functions.h
+++ print_functions.h
@@ -5,7 +5,7 @@
 #include "input_processing.h"
 
 static const char* default_backgr_color = "\033[0m";   // Прозрачный фон
-static const char* backgr_color = "\033[40m";         // Чёрный фон
+static const char* backgr_color = "\033[40m";          // Чёрный фон
 static const char* filename_color = "\033[92m";        // Зелёный текст
 static const char* dirname_color = "\033[94m";         // Синий текст
 
